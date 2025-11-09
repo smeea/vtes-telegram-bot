1c2adc50342a88e5e327591a34e49ca43be60be4
Vadim Matveev <smeea@riseup.net>
2025-11-09 15:41:24 +0300

    add rulings by !cardname

--- app.py
+++ app.py
@@ -9,23 +9,35 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv()
 
 # Get your bot api token from official telegram bot master: @BotFather
-API_KEY = os.environ.get('API_KEY') or "mysecretkey1234567890"
+API_KEY = os.environ.get("API_KEY") or "mysecretkey1234567890"
 
 bot = telebot.TeleBot(API_KEY)
 
-def show_card(message):
-    imagename = './cards/' + cards[0][1] + '.jpg'
-    img = open(imagename, 'rb')
-    bot.send_photo(message.chat.id, img)
+
+def show_ruling(rulings, chatid):
+    list = []
+    for i in rulings:
+        list.append(f"- {i['text']}")
+
+    options = "\n".join(list)
+    bot.send_message(chatid, options)
+
+
+def show_card(cardname, chatid):
+    imagename = "./cards/" + cardname + ".jpg"
+    img = open(imagename, "rb")
+    bot.send_photo(chatid, img)
 
 
 def choose_card(message):
-    if message.text.isdigit() and int(message.text) <= len(cards) and int(
-            message.text) > 0:
+    if (
+        message.text.isdigit()
+        and int(message.text) <= len(cards)
+        and int(message.text) > 0
+    ):
         card_id = int(message.text) - 1
-        imagename = './cards/' + cards[card_id][1] + '.jpg'
-        img = open(imagename, 'rb')
-        bot.send_photo(message.chat.id, img)
+        cardname = cards[card_id][1]
+        show_card(cardname, message.chat.id)
     else:
         get_card_names(message)
 
@@ -33,20 +45,26 @@ def choose_card(message):
 @bot.message_handler()
 def get_card_names(message):
     global cards
-    if re.match('[0-9A-Za-z]{3,}', message.text):
-        cards = get_by_name(message.text)
-        print(cards)
+    if re.match("[!0-9A-Za-z]{3,}", message.text):
+        is_rulings = message.text.startswith("!")
+        text = message.text.removeprefix("!")
+
+        cards = get_by_name(text)
+        print(cards[0][0], cards[0][1])
         if len(cards) == 1:
-            show_card(message)
+            cardname = cards[0][1]
+            show_card(cardname, message.chat.id)
+            if is_rulings:
+                show_ruling(cards[0][2], message.chat.id)
         elif len(cards) > 1:
             cardnames = []
             counter = 0
             for i in cards:
                 counter += 1
-                option = str(counter) + ') ' + i[0]
+                option = str(counter) + ") " + i[0]
                 cardnames.append(option)
 
-            options = '\n'.join(cardnames)
+            options = "\n".join(cardnames)
             bot.send_message(message.chat.id, options)
             bot.register_next_step_handler(message, choose_card)
 
--- search.py
+++ search.py
@@ -4,40 +4,54 @@ import unicodedata
 
 
 def letters_to_ascii(text):
-    return ''.join(c for c in unicodedata.normalize('NFD', text)
-            if unicodedata.category(c) != 'Mn')
+    return "".join(
+        c for c in unicodedata.normalize("NFD", text) if unicodedata.category(c) != "Mn"
+    )
 
 
-with open("cardbase_crypt.json", "r") as crypt_file, open("cardbase_crypt_playtest.json", "r") as crypt_playtest_file:
+with (
+    open("cardbase_crypt.json", "r") as crypt_file,
+    open("cardbase_crypt_playtest.json", "r") as crypt_playtest_file,
+):
     crypt_db = {**json.load(crypt_file), **json.load(crypt_playtest_file)}
     crypt = list(crypt_db.values())
 
-with open("cardbase_lib.json", "r") as library_file, open("cardbase_lib_playtest.json", "r") as library_playtest_file:
+with (
+    open("cardbase_lib.json", "r") as library_file,
+    open("cardbase_lib_playtest.json", "r") as library_playtest_file,
+):
     library_db = {**json.load(library_file), **json.load(library_playtest_file)}
     library = list(library_db.values())
+
 
 def get_by_name(cardname):
     cards = []
     cardname = cardname.lower()
     for card in crypt:
-        if cardname in card['name'].lower() or cardname in letters_to_ascii(
-                card['name'].lower()):
-            name = card['name']
-            cardimagename = letters_to_ascii(re.sub('[\\W]', '', card['name'].lower())) + f"g{card['group']}"
-            if card['new']:
+        if cardname in card["name"].lower() or cardname in letters_to_ascii(
+            card["name"].lower()
+        ):
+            name = card["name"]
+            cardimagename = (
+                letters_to_ascii(re.sub("[\\W]", "", card["name"].lower()))
+                + f"g{card['group']}"
+            )
+            rulings = card["rulings"]
+            if card["new"]:
                 name += f" (G{card['group']})"
 
-            if card['adv'] and card['adv'][0]:
-                cardimagename += 'adv'
-                name += ' (ADV)'
+            if card["adv"] and card["adv"][0]:
+                cardimagename += "adv"
+                name += " (ADV)"
 
-            cards.append([name, cardimagename])
+            cards.append([name, cardimagename, rulings])
 
     for card in library:
-        if cardname in card['name'].lower() or cardname in letters_to_ascii(
-                card['name'].lower()):
-            cardimagename = letters_to_ascii(
-                    re.sub('[\\W]', '', card['name'].lower()))
-            cards.append([card['name'], cardimagename])
+        if cardname in card["name"].lower() or cardname in letters_to_ascii(
+            card["name"].lower()
+        ):
+            cardimagename = letters_to_ascii(re.sub("[\\W]", "", card["name"].lower()))
+            rulings = card["rulings"]
+            cards.append([card["name"], cardimagename, rulings])
 
     return cards
